611ee7fd631527e04aa03d5f416ffe3017dc4af8
Irma-Martinez-Orozco <isc.samuelzarrabalpere@gmail.com>
2020-04-03 18:40:49 -0600

    Tarea

---
+++ app/src/main/java/edu/itsco/holamundoirma/MainActivity.kt
@@ -0,0 +1,17 @@
+package edu.itsco.holamundoirma
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun holamundo(view: View){
+        Toast.makeText(this,"hola mundo, soy irma", Toast.LENGTH_SHORT).show()
+    }
+}
